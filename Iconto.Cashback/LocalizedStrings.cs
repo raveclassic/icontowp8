97719e6103c0a06a67c3fcea73b8acae309080b5
k.agalakov <k.agalakov@iconto.net>
2014-10-22 16:30:49 +0400

    cashback

---
+++ Iconto.Cashback/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Iconto.Cashback.Resources;
+
+namespace Iconto.Cashback
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
